7d663090382e007bd216e3fb37aeb6ee8e0089cd
house <none@none>
2012-11-20 22:56:38 -0500

    hooking process to web

--- index.py
+++ index.py
@@ -5,20 +5,27 @@ from housepy import page, log
 from housepy.crashdb import CrashDB
 from process import process_walk
 
-
-db = CrashDB("walk_data.json")
-
 if page.method == 'POST':
     log.info("Received walk data...")
-    data = json.loads(page.form['walk_data'])  
+    try:
+        data = json.loads(page.form['walk_data'])  
+    except Exception as e:
+        page.error(e)
+        exit()
+    if not len(data):
+        page.error("No data")
+        exit()
+    log.info("data: %s" % data)
     index = int(time.time())
+    db = CrashDB("walk_data.json")
     db[index] = data
-    process_walk(data)
+    db.close()
+    process_walk(index)
     page.text("OK")
 else:
+    db = CrashDB("sequence_data.json")    
     options = {}
     for timestamp in db:
         options[timestamp] = datetime.datetime.fromtimestamp(float(timestamp))
+    db.close()        
     page.render("home.html", {'options': options})
-
-db.close()
