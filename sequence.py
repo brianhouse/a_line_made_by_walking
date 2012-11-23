f847c041b6b5acb93a789ad0f46f5dba842fed61
house <none@none>
2012-11-23 18:41:54 -0500

    progress on sequence loader

---
+++ sequence.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os, time, json, datetime
+from housepy import page, log
+from housepy.crashdb import CrashDB
+from process import process_walk
+
+
+db = CrashDB("sequence_data.json")    
+log.error(db)
+
+try:
+    index = page.form['index']
+    data = db[index]
+    page.json(data)
+except:
+    page.error("Index does not exist")
+
+db.close()
